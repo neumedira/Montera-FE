8641d5cf04e64d9277773d0fa11c1ac66c11228b
chintanajwa2424 <150485929+chintanajwa2424@users.noreply.github.com>
2026-08-21 11:13:04 +0800

    Login sudah tervalidasi ya

---
+++ src/api/axios.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://192.168.1.28:8000/api/v1/",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+});
+
+export default api;
